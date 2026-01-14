62a873c3e65870192e710b50441c6d2594f1674b
JaxX <contact@anunaqi.com>
2026-01-14 03:26:17 +0100

    fix: add search icon to material symbols import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="fr" suppressHydrationWarning>
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=distance,verified" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=distance,search,verified" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
